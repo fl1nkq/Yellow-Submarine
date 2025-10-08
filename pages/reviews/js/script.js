d6905119a575b688d7ba3c29fc1c163ac78588d2
fl1nkq <evanas.daujotas@stud.techin.lt>
2025-10-08 19:20:41 +0300

    media rew

--- pages/reviews/js/script.js
+++ pages/reviews/js/script.js
@@ -1,33 +1,66 @@
-let text = document.getElementsByClassName("customer-box");
-let arrows = document.getElementsByClassName("strelki");
+let strelki = document.getElementsByClassName("strelki");
+let cards = document.getElementsByClassName("customer-box");
+let i = 0
+console.log(strelki, cards);
 
-let pokaz = 0;
+for(let card of cards){
+    card.style.opacity = "0";
+    card.style.position = "absolute";
+    card.style.zIndex = "-1";
+    card.style.width = "0";
 
-function text_style_off_0(){
-    for(let i = 0; i < text.length; i++){
-        text[i].style.opacity = "0";
-        text[i].style.position = "absolute";
-    }
-}
-
-function text_style_on_0(pokaz){
-    text[pokaz].style.opacity = "1";
-    text[pokaz].style.position = "relative";
-}
-
-text_style_off_0();
-text_style_on_0(pokaz);
-
-for(let i = 0; i < arrows.length; i++){
-    arrows[i].addEventListener("click", function(){
-        text_style_off_0();
-        if(i == 0){
-            pokaz = (pokaz - 1 + text.length) % text.length;
-        }
-        else{
-            pokaz = (pokaz + 1) % text.length;
-        }
-
-        text_style_on_0(pokaz);
-    })
 };
+
+cards[0].style.opacity = "1";
+cards[0].style.position = "relative";
+cards[0].style.zIndex = "0";
+cards[0].style.width = "100%";
+
+
+strelki[0].addEventListener("click", function(){
+    cards[i].style.opacity = "0";
+    cards[i].style.zIndex = "-1";
+    cards[i].style.position = "absolute";
+    cards[i].style.width = "0%";
+
+    i = (i - 1 + cards.length) % cards.length;
+
+    cards[i].style.position = "relative";
+    cards[i].style.opacity = "1";
+    cards[i].style.zIndex = "0";
+    cards[i].style.width = "100%";
+})
+
+strelki[1].addEventListener("click", function(){
+    cards[i].style.opacity = "0";
+    cards[i].style.zIndex = "-1";
+    cards[i].style.position = "absolute";
+    cards[i].style.width = "0%";
+
+    i = (i + 1) % cards.length;
+
+    cards[i].style.position = "relative";
+    cards[i].style.opacity = "1";
+    cards[i].style.zIndex = "0";
+    cards[i].style.width = "100%";
+})
+
+
+window.onscroll = function(){
+    let scrl_btn = document.getElementById("scrl_up_btn");
+    if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
+        scrl_btn.style.opacity = "1";
+        scrl_btn.style.zIndex = "10";
+    }
+    else{
+        scrl_btn.style.opacity = "0";
+        scrl_btn.style.zIndex = "-1";
+    };
+};
+
+document.getElementById("scrl_up_btn").addEventListener("click", function(){
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
